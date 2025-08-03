54087606ac51f75638e7f7053501cb9460363e94
freeutka <freeutka@inbox.lv>
2025-08-03 11:13:12 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PORT=${SERVER_PORT:-8080}
-
-envsubst '$PORT' < /home/container/default.conf.template > /home/container/default.conf
-
-nginx -c /home/container/default.conf -g "daemon off;"
